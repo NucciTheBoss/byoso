9f79265646a45dfa3f7e33d4511bf866a1894074
NucciTheBoss <jcn23@psu.edu>
2021-07-11 16:30:54 -0400

    Add byoso source code

---
+++ byoso.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import RPi.GPIO as gp
+from flask import Flask, render_template
+
+
+# Initialize Flask application
+app = Flask(__name__)
+
+# Initialize RPi.GPIO
+gp.setmode (gp.BCM)
+gp.setwarnings(False)
+outlet_off = [True]
+
+# Initialize pin 17
+gp.setup(pin, gp.OUT)
+gp.output(pin, gp.LOW)
+
+# For when the byoso is first accessed via the web browser
+@app.route("/")
+def home():
+    return render_template("on.html")
+
+
+@app.route("/on")
+def turn_on():
+    if outlet_off[0] is True:
+        outlet_off[0] = False
+        gp.output(pin, gp.HIGH)
+        render_template("off.html")
+
+    else:
+        render_template("on.html")
+
+
+@app.route("/off")
+def turn_off():
+    if outlet_off is False:
+        outlet_off[0] = True
+        gp.output(pin, gp.LOW)
+        render_template("on.html")
+
+    else:
+        render_template("off.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
